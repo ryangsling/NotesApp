53c1367d72ad85dcde47ce1111ec17c804412eb5
ryangsling <jamesahsan1371@gmail.com>
2026-07-20 18:45:16 +0600

    Add Struct2Test generated Espresso test and CI workflow

---
+++ app/src/androidTest/java/com/example/app/Struct2TestGeneratedTest.java
@@ -0,0 +1,38 @@
+package com.example.notesapp;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class Struct2TestGeneratedTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void generatedFromHierarchy_homeScreenTest() {
+        onView(withId(R.id.search_src_text))
+                .check(matches(isDisplayed()))
+                .perform(click(), replaceText("test"), closeSoftKeyboard());
+
+        pressBack();
+
+        onView(withId(R.id.fab_add))
+                .check(matches(isDisplayed()))
+                .perform(click());
+    }
+}
